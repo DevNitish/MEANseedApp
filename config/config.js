94d9bd06a2ed6a40adafbc2c9f411573894885a8
Nitish Kumar <nitishonspeed@gmail.com>
2017-02-06 23:30:53 +0530

    fixed errors

--- config/config.js
+++ config/config.js
@@ -2,10 +2,9 @@
 Change the name myDb to your database name
 
 */
-//var connectionstring = 'mongodb://localhost:27017/myDb';
+var connectionstring = 'mongodb://localhost:27017/myDb';
 exports.serverOptions = {
      'auto_reconnect': true,
      'poolSize': 10
  };
-var connectionstring = "mongodb://nearu:nearu@ds051843.mongolab.com:51843/ultivoz";
 exports.mongoconnectionstring = connectionstring;
