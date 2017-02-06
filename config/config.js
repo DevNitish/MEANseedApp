7c8943b4d49bb8bf7143e195646ebc98eba0bff2
devnitish <nitishonspeed@gmail.com>
2017-02-06 18:55:08 +0530

    testing

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@
 Change the name myDb to your database name
 
 */
-var connectionstring = 'mongodb://localhost:27017/myDb';
+//var connectionstring = 'mongodb://localhost:27017/myDb';
 exports.serverOptions = {
      'auto_reconnect': true,
      'poolSize': 10
  };
-//var connectionstring = "mongodb://nearu:nearu@ds051843.mongolab.com:51843/ultivoz";
+var connectionstring = "mongodb://nearu:nearu@ds051843.mongolab.com:51843/ultivoz";
 
 exports.mongoconnectionstring = connectionstring;
