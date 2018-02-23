9a7ce0d1eaf21da0213f1531210e91e0870c88bc
rajkeshav544@gmail.com <rajkeshav544@gmail.com>
2018-02-24 00:27:14 +0530

    first commit

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@
 Change the name myDb to your database name
 
 */
-var connectionstring = 'mongodb://localhost:27017/myDb';//Change myDb to yourDBname
+//var connectionstring = 'mongodb://localhost:27017/myDb';//Change myDb to yourDBname
 exports.serverOptions = {
      'auto_reconnect': true,
      'poolSize': 10
  };
-//var connectionstring = "mongodb://nearu:nearu@ds051843.mongolab.com:51843/ultivoz";
+var connectionstring = "mongodb://nearu:nearu@ds051843.mongolab.com:51843/ultivoz";
 exports.mongoconnectionstring = connectionstring;
